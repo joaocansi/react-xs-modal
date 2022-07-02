5ce61a09548212e6a823b79e0d8f9efa04d3059c
joaocansi <whitshs2003@hotmail.com>
2022-07-02 18:09:39 -0300

    fix: react and react-dom added to devDependencies

--- __test__/asModal.test.tsx
+++ __test__/asModal.test.tsx
@@ -1,62 +1,62 @@
-import React from "react";
+import React from 'react'
 
-import { ModalProvider } from "../src/useModal";
-import { fireEvent, render } from "@testing-library/react";
-import { asModal } from "../src/asModal";
-import { useModal } from "../src/useModal";
+import { ModalProvider } from '../src/useModal'
+import { fireEvent, render } from '@testing-library/react'
+import { asModal } from '../src/asModal'
+import { useModal } from '../src/useModal'
 
 export const ModalTestComponent = ({ closeOnClick = false }) => {
-  const { displayModal, hideModal } = useModal();
-  const Modal = asModal(() => <span></span>, { closeOnClick });
+  const { displayModal, hideModal } = useModal()
+  const Modal = asModal(() => <span></span>, { closeOnClick })
 
   return (
     <>
       <button onClick={() => displayModal(Modal)}>open</button>
       <button onClick={hideModal}>close</button>
     </>
-  );
-};
+  )
+}
 
-describe("asModal", () => {
-  it("should close when clicking outside modal content if closeOnClick is true", () => {
+describe('asModal', () => {
+  it('should close when clicking outside modal content if closeOnClick is true', () => {
     const { container, getByText } = render(
       <ModalProvider>
         <ModalTestComponent closeOnClick={true} />
-      </ModalProvider>
-    );
+      </ModalProvider>,
+    )
 
-    fireEvent.click(getByText("open"));
-    fireEvent.click(container.querySelector("div#modal-overlay") as Element);
+    fireEvent.click(getByText('open'))
+    fireEvent.click(container.querySelector('div#modal-overlay') as Element)
 
-    expect(container.querySelector("div#modal-overlay")).toBeFalsy();
-    expect(container.querySelector("div#modal-content")).toBeFalsy();
-  });
+    expect(container.querySelector('div#modal-overlay')).toBeFalsy()
+    expect(container.querySelector('div#modal-content')).toBeFalsy()
+  })
 
-  it("should not close when clicking inside modal content if closeOnClick is true", () => {
+  it('should not close when clicking inside modal content if closeOnClick is true', () => {
     const { container, getByText } = render(
       <ModalProvider>
         <ModalTestComponent closeOnClick={true} />
-      </ModalProvider>
-    );
+      </ModalProvider>,
+    )
 
-    fireEvent.click(getByText("open"));
-    fireEvent.click(container.querySelector("div#modal-content") as Element);
+    fireEvent.click(getByText('open'))
+    fireEvent.click(container.querySelector('div#modal-content') as Element)
 
-    expect(container.querySelector("div#modal-overlay")).toBeTruthy();
-    expect(container.querySelector("div#modal-content")).toBeTruthy();
-  });
+    expect(container.querySelector('div#modal-overlay')).toBeTruthy()
+    expect(container.querySelector('div#modal-content')).toBeTruthy()
+  })
 
-  it("should not close when clicking outside modal content if closeOnClick is false", () => {
+  it('should not close when clicking outside modal content if closeOnClick is false', () => {
     const { container, getByText } = render(
       <ModalProvider>
         <ModalTestComponent closeOnClick={false} />
-      </ModalProvider>
-    );
+      </ModalProvider>,
+    )
 
-    fireEvent.click(getByText("open"));
-    fireEvent.click(container.querySelector("div#modal-overlay") as Element);
+    fireEvent.click(getByText('open'))
+    fireEvent.click(container.querySelector('div#modal-overlay') as Element)
 
-    expect(container.querySelector("div#modal-overlay")).toBeTruthy();
-    expect(container.querySelector("div#modal-content")).toBeTruthy();
-  });
-});
+    expect(container.querySelector('div#modal-overlay')).toBeTruthy()
+    expect(container.querySelector('div#modal-content')).toBeTruthy()
+  })
+})
