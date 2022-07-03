4140101a2694a507f62cbd1f93f2e307715260f2
joaocansi <whitshs2003@hotmail.com>
2022-07-02 22:09:31 -0300

    fix: handleOnClickOutside recreated

--- src/asModal.tsx
+++ src/asModal.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentType, CSSProperties, useEffect, useRef } from 'react'
+import React, { ComponentType, CSSProperties, useEffect } from 'react'
 import { useModal } from './useModal'
 
 interface AsModalOptionsProps {
@@ -7,21 +7,17 @@ interface AsModalOptionsProps {
     contentStyle?: CSSProperties
   }
   closeOnClick?: boolean
-  allowScroll?: boolean
 }
 
 export function asModal<T>(Component: ComponentType<T>, options: AsModalOptionsProps) {
   return function (props: T) {
     const { hideModal } = useModal()
-    const contentRef = useRef<HTMLDivElement>(null)
 
     const handleOnClickOutside = (event: MouseEvent) => {
       const target = event.target as HTMLElement
-      const overlay = document.getElementById('modal-overlay')
+      const modalOverlay = document.getElementById('modal-overlay')
 
-      if (contentRef.current && !contentRef.current.contains(target) && overlay?.contains(target)) {
-        hideModal()
-      }
+      if (target.contains(modalOverlay)) hideModal()
     }
 
     useEffect(() => {
@@ -38,7 +34,7 @@ export function asModal<T>(Component: ComponentType<T>, options: AsModalOptionsP
 
     return (
       <div id='modal-overlay' style={options.style?.overlayStyle}>
-        <div ref={contentRef} id='modal-content' style={options.style?.contentStyle}>
+        <div id='modal-content' style={options.style?.contentStyle}>
           <Component {...props} />
         </div>
       </div>
